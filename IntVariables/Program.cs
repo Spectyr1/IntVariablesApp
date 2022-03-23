971cf6bde8a82fd6f6b6de14b65243b97e9d6319
Danny Warren <danny@theforbiddenarts.com>
2022-03-22 21:51:45 -0400

    Final code

--- IntVariables/Program.cs
+++ IntVariables/Program.cs
@@ -1,4 +1,21 @@
 ﻿
+// Yes - 2, 18, 2854, -9
+// No - 1.2, =5.7
 
+int age = 0;
 
+age = 43;
 
+int ageInTenYears = age + 10;
+
+// Two Billion +/-
+// signed Int32
+// unsigned - 4 billion
+// bit - 0 ro 1
+// Byte - 8 bits - 00000000
+// 1, 11, 111
+
+Console.WriteLine(age);
+Console.WriteLine(ageInTenYears);
+
+// Dont divide ints - Console.WriteLine(age / 2);
